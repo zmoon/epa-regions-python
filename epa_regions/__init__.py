140ae1228821bb02bb0371cada236ec17ca1e750
zmoon <zmoon92@gmail.com>
2024-01-25 15:20:14 -0700

    To package

--- epa_regions/__init__.py
+++ epa_regions/__init__.py
@@ -1,3 +1,7 @@
+"""
+EPA region definitions and representations in
+regionmask and GeoPandas.
+"""
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
@@ -169,33 +173,7 @@ def get_regions_regionmask(*, resolution: str = "50m") -> Regions:
         numbers="number",
         names="name_",
         abbrevs="abbrev_",
+        overlap=False,
     )
 
     return regions_rm
-
-
-if __name__ == "__main__":
-    import itertools
-
-    import matplotlib.pyplot as plt
-
-    # Some checks
-    for ((n1, _), states1), ((n2, _), states2) in itertools.combinations(
-        regions.items(), 2
-    ):
-        states1_set = set(states1)
-        states2_set = set(states2)
-        assert len(states1) == len(states1_set), f"R{n1} has duplicates"
-        assert len(states2) == len(states2_set), f"R{n2} has duplicates"
-        assert not states1_set & states2_set, f"R{n1} and R{n2} share constituents"
-
-    regions_gp = get_regions_geopandas()
-    regions_rm = get_regions_regionmask()
-
-    fig, ax = plt.subplots(constrained_layout=True, figsize=(6, 3.1))
-    regions_gp.plot(column="number", cmap="tab10", legend=False, ax=ax)
-
-    fig = plt.figure(constrained_layout=True, figsize=fig.get_size_inches())
-    regions_rm.plot(add_label=True)
-
-    plt.show()
