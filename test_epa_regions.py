a4f339a635524caae5c73c9481ceaab77428fec9
zmoon <zmoon92@gmail.com>
2024-01-29 16:01:47 -0700

    Test get

--- test_epa_regions.py
+++ test_epa_regions.py
@@ -1,9 +1,22 @@
-from epa_regions import regions
-from epa_regions.load import VERSIONS
+import itertools
+
+import geopandas as gpd
+import pytest
+
+from epa_regions import get, regions, to_regionmask
+from epa_regions.load import RESOLUTIONS, VERSIONS
+
+versions_test = ["v4.1.0", "v5.0.0", "v5.1.2"]
+
+
+def test_versions_test_ok():
+    assert set(versions_test) <= set(VERSIONS)
+    assert versions_test[-1] == VERSIONS[-1]
 
 
 def test_regions_def():
-    import itertools
+    assert len(regions) == 10
+    # TODO: keys == list(range(1, 11))
 
     for ((n1, _), states1), ((n2, _), states2) in itertools.combinations(
         regions.items(), 2
@@ -42,3 +55,32 @@ def test_ne_s3_versions():
     assert len(versions_set) == len(VERSIONS), "should be unique"
 
     assert versions_set == se_versions_set
+
+
+@pytest.mark.parametrize(
+    "resolution, version",
+    list(itertools.product(RESOLUTIONS, versions_test)),
+)
+def test_get(resolution, version):
+    gdf = get(resolution=resolution, version=version)
+    assert isinstance(gdf, gpd.GeoDataFrame)
+    assert len(gdf) == len(regions) == 10
+    assert list(gdf) == [
+        "epa_region",
+        "geometry",
+        "number",
+        "constituents",
+        "constituent_names",
+        "epa_region_office",
+    ]
+
+    rm = to_regionmask(gdf)
+    assert len(rm) == 10
+
+
+def test_get_invalid():
+    with pytest.raises(ValueError, match="resolution must be one of"):
+        get(resolution="invalid", version="v5.1.2")
+
+    with pytest.raises(ValueError, match="version must be one of"):
+        get(resolution="50m", version="invalid")
