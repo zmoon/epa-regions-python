6817ef6310ea8f14c3cf7e3d6c7d76013d800785
zmoon <zmoon92@gmail.com>
2024-01-29 16:27:20 -0700

    Increment

--- epa_regions/__init__.py
+++ epa_regions/__init__.py
@@ -9,7 +9,7 @@ from typing import TYPE_CHECKING, Final, NamedTuple
 
 logger = logging.getLogger(__name__)
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 if TYPE_CHECKING:
     from geopandas import GeoDataFrame
