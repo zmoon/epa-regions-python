c75923378b0af2d964c40961ea3b257878f7ab1b
zmoon <zmoon92@gmail.com>
2024-01-29 15:48:38 -0700

    Fix tests; CLI res option + color labels

--- epa_regions/__main__.py
+++ epa_regions/__main__.py
@@ -1,27 +1,46 @@
-import itertools
-# import logging; logging.basicConfig(level="INFO")
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--info", action="store_true")
+parser.add_argument("--resolution", "-r", default="10m")
+
+args = parser.parse_args()
+
+if args.info:
+    import logging
+
+    logging.basicConfig(level="INFO")
 
 import matplotlib.pyplot as plt
+import numpy as np
 
-from . import regions, get_regions_geopandas, get_regions_regionmask
+from . import get
 
-# Some checks
-for ((n1, _), states1), ((n2, _), states2) in itertools.combinations(
-    regions.items(), 2
-):
-    states1_set = set(states1)
-    states2_set = set(states2)
-    assert len(states1) == len(states1_set), f"R{n1} has duplicates"
-    assert len(states2) == len(states2_set), f"R{n2} has duplicates"
-    assert not states1_set & states2_set, f"R{n1} and R{n2} share constituents"
+regions = get(resolution=args.resolution)
 
-regions_gp = get_regions_geopandas()
-regions_rm = get_regions_regionmask()
+fig, ax = plt.subplots(constrained_layout=True, figsize=(8, 4))
+regions.plot(column="number", cmap="tab10", ax=ax)
 
-fig, ax = plt.subplots(constrained_layout=True, figsize=(6, 3.1))
-regions_gp.plot(column="number", cmap="tab10", legend=False, ax=ax)
+xs = np.linspace(0.8, 0.98, 3)
+ys = np.linspace(0.98, 0.75, 4)
 
-fig = plt.figure(constrained_layout=True, figsize=fig.get_size_inches())
-regions_rm.plot(add_label=True)
+i = 1
+for y in ys:
+    for x in xs:
+        if i == 10 and not x == xs[-1]:
+            continue
+        ax.text(
+            x,
+            y,
+            f"R{i}",
+            ha="right",
+            va="top",
+            transform=ax.transAxes,
+            fontsize=13,
+            color=plt.cm.tab10.colors[i - 1],
+            # weight="bold",
+        )
+        i += 1
 
 plt.show()
--- test_epa_regions.py
+++ test_epa_regions.py
@@ -1,4 +1,18 @@
-from t import VERSIONS
+from epa_regions import regions
+from epa_regions.load import VERSIONS
+
+
+def test_regions_def():
+    import itertools
+
+    for ((n1, _), states1), ((n2, _), states2) in itertools.combinations(
+        regions.items(), 2
+    ):
+        states1_set = set(states1)
+        states2_set = set(states2)
+        assert len(states1) == len(states1_set), f"R{n1} has duplicates"
+        assert len(states2) == len(states2_set), f"R{n2} has duplicates"
+        assert not states1_set & states2_set, f"R{n1} and R{n2} share constituents"
 
 
 def test_ne_s3_versions():
